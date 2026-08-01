70adc42b34146cf93fd6dc237db818274e29aadd
Clara <macfernandez209@gmail.com>
2026-08-01 18:39:30 -0300

    boton deshacer

--- script.js
+++ script.js
@@ -13,6 +13,6 @@ function agregarALaPantalla(value) {
   }
   
   function deshacer() {
-    const valorPantalla = document.getElementById('pantalla').value;
-    document.getElementById('pantalla').value= valorPantalla.slice(0, -1);
+    const pantalla = document.getElementById('pantalla');
+    pantalla.value = pantalla.value.slice(0, -1);
   }
